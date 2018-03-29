c398722405395576972aa36b82b8b95bdfc1d006
rahulojha16@gmail.com <rahulojha16@gmail.com>
2018-03-29 20:32:12 +0530

    added reg page

--- src/App.js
+++ src/App.js
@@ -6,11 +6,7 @@ import Registration from './components/registration';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+      <div>
         <Registration />
       </div>
     );
--- src/components/registration.js
+++ src/components/registration.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import '../assets/registrations.css';
 
 export default class Registration extends Component {
 
@@ -11,19 +12,41 @@ export default class Registration extends Component {
     event.preventDefault();
     const data = new FormData(event.target);
 
-    fetch('http://localhost:3001/auth', {
+    fetch('http://localhost:3000/auth', {
       method: 'POST',
       body: data,
+    }).then((response) => {
+      if ( response.ok) {
+        alert('Registration Succesfull');
+      }
     });
   }
 
   render() {
     return(
-      <div className='form'>
+      <div className='form col-md-4'>
        <form onSubmit={this.handleSubmit}>
+
+        <div className="form-group">
          <label htmlFor='email'>Email</label>
-         <input type='text' name='email'/>
-         <button> submit </button>
+         <input type='text' name='email' className="form-control" />
+        </div>
+
+        <div className="form-group">
+         <label htmlFor='Password'>Password</label>
+         <input type='text' name='password' className="form-control" />
+        </div>
+
+        <div className="form-group">
+         <label htmlFor='Password Confirmation'>Password confirmation</label>
+         <input type='text' name='password_confirmation' className="form-control" />
+        </div>
+        <div className="form-group">
+         <label htmlFor='name'>Name</label>
+         <input type='text' name='name' className="form-control" />
+        </div>
+
+        <button> submit </button>
        </form>
       </div>
     )
